f6d891aa77f62b9729fd7031a69e0c60fe6d6828
unknown <eske17.ea@gmail.com>
2023-07-01 13:27:20 +0300

    navigation to different screen

--- app/checkout/delivery.tsx
+++ app/checkout/delivery.tsx
@@ -1,12 +1,20 @@
-import { Link } from "expo-router";
+import { Link, useRouter } from "expo-router";
 import { View, Text } from "react-native";
+import { Button } from "react-native-paper";
 
 export default function DeliveryDetails() {
+  const router = useRouter();
+
+  const nextPage = () => {
+    router.push("/checkout/payment");
+  };
+
   return (
     <View>
       <Text>Delivery Details</Text>
-
-      <Link href={"/checkout/payment"}>Next</Link>
+      <Button onPress={nextPage} mode="contained">
+        Next
+      </Button>
     </View>
   );
 }
--- app/checkout/payment.tsx
+++ app/checkout/payment.tsx
@@ -1,9 +1,19 @@
+import { useRouter } from "expo-router";
 import { View, Text } from "react-native";
+import { Button } from "react-native-paper";
 
 export default function PaymentDetails() {
+  const router = useRouter();
+
+  const nextPage = () => router.push("/");
+
   return (
     <View>
       <Text>Payment Details</Text>
+
+      <Button onPress={nextPage} mode="contained">
+        Submit
+      </Button>
     </View>
   );
 }
