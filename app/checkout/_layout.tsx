faa12e10d960a278b9011d32ee030b4704a24985
unknown <eske17.ea@gmail.com>
2023-07-01 12:27:10 +0300

    checkout form project structure

---
+++ app/checkout/_layout.tsx
@@ -0,0 +1,31 @@
+import { Stack } from "expo-router";
+import { StatusBar } from "expo-status-bar";
+
+export default function CheckoutStack() {
+  return (
+    <>
+      <StatusBar style="light" />
+      <Stack
+        screenOptions={{
+          contentStyle: { padding: 15, backgroundColor: "#F0E8F8", flex: 1 },
+          headerStyle: { backgroundColor: "#673AB8" },
+          headerTitleStyle: { color: "white" },
+          headerTintColor: "#F0E8F8",
+        }}
+      >
+        <Stack.Screen
+          name="index"
+          options={{ title: "Personal Information" }}
+        />
+        <Stack.Screen
+          name="delivery"
+          options={{ title: "Delivery Information" }}
+        />
+        <Stack.Screen
+          name="payment"
+          options={{ title: "Payment Information" }}
+        />
+      </Stack>
+    </>
+  );
+}
